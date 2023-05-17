ce97861aed26706a8b3cd9f2ad870a124a5d540e
Bruno Gonçalves <bruuno_goncalves@outlook.com.br>
2023-05-17 14:05:31 -0300

    feat: add animation about

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,14 @@ window.onscroll = () => {
         links.classList.remove('active');
         document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
       });
-    };
+      // active sections for animations on scroll
+      sec.classList.add('show-animate');
+    }
+    // if want to use animation that repeats on scroll use this
+    else {
+      sec.classList.remove('show-animate');
+
+    }
   });
 
   // sticky header
